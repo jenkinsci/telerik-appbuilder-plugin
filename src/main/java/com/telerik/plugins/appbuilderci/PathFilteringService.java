0697c843fc03a31186e3904407d2416d82714721
Georgi Alexandrov <georgi.alexandrov@telerik.com>
2015-11-30 17:17:00 +0200

    Fix Filtering Service

--- src/main/java/com/telerik/plugins/appbuilderci/PathFilteringService.java
+++ src/main/java/com/telerik/plugins/appbuilderci/PathFilteringService.java
@@ -6,21 +6,24 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathFilteringService {
-	private Stream<PathMatcher> ignoreFilesRules;
+	private List<PathMatcher> ignoreFilesRules;
 
 	public PathFilteringService(Path ignoreRulesFilePath) throws IOException {
 		if (Files.exists(ignoreRulesFilePath)) {
 			FileSystem fs = FileSystems.getDefault();			
-			this.ignoreFilesRules = Files.lines(ignoreRulesFilePath).map(path -> fs.getPathMatcher("glob:" + path));
+			this.ignoreFilesRules = Files.lines(ignoreRulesFilePath)
+					.map(path -> fs.getPathMatcher("glob:" + path))
+					.collect(Collectors.toList());
 		}
 	}
 
 	public boolean isFileExcluded(Path path) {
 		if(this.ignoreFilesRules != null){
-			return this.ignoreFilesRules.anyMatch(rule -> rule.matches(path));
+			return this.ignoreFilesRules.stream().anyMatch(rule -> rule.matches(path));
 		}
 		return false;
 	}
