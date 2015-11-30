f3991642fc04ab25674abd5a9ebf3edf8b2dc730
Georgi Alexandrov <georgi.alexandrov@telerik.com>
2015-11-30 22:18:59 +0200

    Downgrade to Java7

--- src/main/java/com/telerik/plugins/appbuilderci/PathFilteringService.java
+++ src/main/java/com/telerik/plugins/appbuilderci/PathFilteringService.java
@@ -6,24 +6,38 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class PathFilteringService {
-	private List<PathMatcher> ignoreFilesRules;
+	private List<PathMatcher> ignoreFilesRules = new ArrayList<PathMatcher>();
 
 	public PathFilteringService(Path ignoreRulesFilePath) throws IOException {
 		if (Files.exists(ignoreRulesFilePath)) {
-			FileSystem fs = FileSystems.getDefault();			
-			this.ignoreFilesRules = Files.lines(ignoreRulesFilePath)
-					.map(path -> fs.getPathMatcher("glob:" + path))
-					.collect(Collectors.toList());
+			final FileSystem fs = FileSystems.getDefault();		
+			Object[] list = Files.lines(ignoreRulesFilePath)
+					.map(new Function<String, PathMatcher>() {
+						@Override
+						public PathMatcher apply(String path) {
+							return fs.getPathMatcher("glob:" + path);
+						}
+					}).toArray();
+			for (Object pathMatcher : list) {
+				ignoreFilesRules.add((PathMatcher)pathMatcher);
+			}
 		}
 	}
 
-	public boolean isFileExcluded(Path path) {
+	public boolean isFileExcluded(final Path path) {
 		if(this.ignoreFilesRules != null){
-			return this.ignoreFilesRules.stream().anyMatch(rule -> rule.matches(path));
+			return this.ignoreFilesRules.stream().anyMatch(new Predicate<PathMatcher>() {
+				@Override
+				public boolean test(PathMatcher rule) {
+					return rule.matches(path);
+				}
+			});
 		}
 		return false;
 	}
